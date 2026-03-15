99ee3135a6c7ff55315641207a349702553ba083
Winnie <epanovaalina@gmail.com>
2026-03-15 23:58:12 +0500

    Add files via upload

---
+++ lab_4_1.fs
@@ -0,0 +1,82 @@
+﻿// Лабораторная №4
+// Бинарные деревья заполняются случайными строками.
+// Дерево содержит строки. В конец каждой строки приписать ее же
+
+open System
+
+type BinaryTree = 
+    | Node of string * BinaryTree * BinaryTree
+    | Empty
+
+// Генератор случайных чисел
+let rnd = Random()
+
+// Генерация случайной строки
+let randomString () =
+    let letters = "abcde"
+    let length = rnd.Next(3,6) // От 3 до 5 символов
+    
+    let rec buildString n = 
+        if n = 0 then 
+            "" // Возвращается пустая строка
+        else 
+            string letters.[rnd.Next(letters.Length)] + buildString (n - 1)
+
+    buildString length
+
+// Создание случайного дерева
+let rec createTree depth =
+    if depth = 0 then
+        Empty
+    else
+        Node(
+            randomString(),
+            createTree (depth - 1),
+            createTree (depth - 1)
+        )
+
+// map для дерева
+let rec mapTree f tree =
+    match tree with
+    | Empty -> Empty
+    | Node(data,left,right) ->
+        Node(
+            f data,
+            mapTree f left,
+            mapTree f right
+        )
+
+// Функция удвоения строки
+let duplicateString s =
+    s + s
+
+// Симметричный обход дерева 
+let rec printTree tree = 
+    match tree with 
+    | Node (data, left, right) ->
+        printTree left
+        printfn "%s" data
+        printTree right
+    | Empty -> ()
+
+
+[<EntryPoint>]
+let main args =
+
+    printf "Введите глубину дерева: "
+    let n = int(Console.ReadLine())
+    
+    if n < 0 then
+        printfn "Ошибка! Глубина дерева должна быть положительна!"
+    else 
+        let tree = createTree n
+
+        printfn "Исходное дерево:"
+        printTree tree
+
+        let newTree = mapTree duplicateString tree
+
+        printfn "\nНовое дерево:"
+        printTree newTree
+
+    0
---
+++ lab_4_2.fs
@@ -0,0 +1,68 @@
+﻿// Лабораторная №4
+// Задача 2
+// Выяснить, содержится ли в дереве элемент с заданным значением.
+
+open System
+
+type BinaryTree =
+    | Node of int * BinaryTree * BinaryTree
+    | Empty
+
+let rnd = Random()
+
+// Создание дерева со случайными числами
+let rec createTree depth =
+    if depth = 0 then
+        Empty
+    else
+        Node(
+            rnd.Next(1,100),  // От 1 до 99
+            createTree (depth - 1),
+            createTree (depth - 1)
+        )
+
+// Симметричный обход дерева 
+let rec printTree tree =
+    match tree with
+    | Node(data,left,right) ->
+        printTree left
+        printf "%d " data
+        printTree right
+    | Empty -> ()
+
+// fold для дерева
+let rec foldTree f acc tree =
+    match tree with
+    | Empty -> acc
+    | Node(data,left,right) ->
+        let processLeft = foldTree f acc left
+        let current = f processLeft data
+        foldTree f current right
+
+// Проверка наличия элемента
+let includeValue value tree =
+    foldTree (fun found x -> found || (x = value)) false tree
+
+[<EntryPoint>]
+let main args =
+
+    printf "Введите глубину дерева: "
+    let n = int(Console.ReadLine())
+    
+    if n < 0 then
+        printfn "Ошибка! Глубина дерева должна быть положительна!"
+    else 
+        let tree = createTree n
+
+        printfn "Дерево:"
+        printTree tree
+
+        printf "\nВведите число для поиска: "
+        let value = int(Console.ReadLine())
+
+        if includeValue value tree then
+            printfn "Число найдено"
+        else
+            printfn "Число не найдено"
+
+    0
