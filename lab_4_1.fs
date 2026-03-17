e396fa252949b477f97215ce6312aaa18a52ad2f
Winnie <epanovaalina@gmail.com>
2026-03-17 22:04:30 +0500

    Update lab_4_1.fs

--- lab_4_1.fs
+++ lab_4_1.fs
@@ -1,44 +1,41 @@
-﻿// Лабораторная №4
-// Бинарные деревья заполняются случайными строками.
-// Дерево содержит строки. В конец каждой строки приписать ее же
+// Лабораторная №4
+// Задание 1
+// Дерево содержит строки. В конец каждой строки приписать ее же.
 
 open System
 
-type BinaryTree = 
-    | Node of string * BinaryTree * BinaryTree
-    | Empty
+type 't BinaryTree = 
+    Node of 't * 't BinaryTree * 't BinaryTree
+    | Nil
 
-// Генератор случайных чисел
 let rnd = Random()
 
 // Генерация случайной строки
 let randomString () =
-    let letters = "abcde"
-    let length = rnd.Next(3,6) // От 3 до 5 символов
-    
-    let rec buildString n = 
-        if n = 0 then 
-            "" // Возвращается пустая строка
+    let length = rnd.Next(3, 6)
+
+    let rec build n =
+        if n = 0 then ""
+        else
+            let c = char (rnd.Next(97, 123))
+            string c + build (n - 1)
+
+    build length
+
+// Функция вставки
+let rec insert t x = 
+    match t with
+    | Nil -> Node (x, Nil, Nil)
+    | Node(z,L,R) ->
+        if x < z then
+            Node(z, insert L x, R)
         else 
-            string letters.[rnd.Next(letters.Length)] + buildString (n - 1)
-
-    buildString length
-
-// Создание случайного дерева
-let rec createTree depth =
-    if depth = 0 then
-        Empty
-    else
-        Node(
-            randomString(),
-            createTree (depth - 1),
-            createTree (depth - 1)
-        )
+            Node(z, L, insert R x)
 
 // map для дерева
 let rec mapTree f tree =
     match tree with
-    | Empty -> Empty
+    | Nil -> Nil
     | Node(data,left,right) ->
         Node(
             f data,
@@ -46,37 +43,47 @@ let rec mapTree f tree =
             mapTree f right
         )
 
-// Функция удвоения строки
-let duplicateString s =
+// Удвоение строки
+let duplicateString s = 
     s + s
 
-// Симметричный обход дерева 
-let rec printTree tree = 
-    match tree with 
-    | Node (data, left, right) ->
-        printTree left
-        printfn "%s" data
-        printTree right
-    | Empty -> ()
+// Вывод дерева
+let spaces n = List.fold (fun s _ -> s+ "    ") "" [0..n]
 
+let printTree tree =
+    let rec print t level =
+        match t with
+        | Node(data, left, right) ->
+            print left (level + 1)
+            printfn "%s%s" (spaces level) data
+            print right (level + 1)
+        | Nil -> ()
+    print tree 0
 
 [<EntryPoint>]
 let main args =
 
-    printf "Введите глубину дерева: "
+    printf "Количество элементов: "
     let n = int(Console.ReadLine())
-    
-    if n < 0 then
-        printfn "Ошибка! Глубина дерева должна быть положительна!"
-    else 
-        let tree = createTree n
 
-        printfn "Исходное дерево:"
-        printTree tree
+    // Создание списка
+    let A =
+        [ 
+        for i in 1..n -> 
+            randomString() 
+        ]
 
-        let newTree = mapTree duplicateString tree
+    printfn "Исходный список: %A" A
 
-        printfn "\nНовое дерево:"
-        printTree newTree
+    // Построение дерева
+    let tree = List.fold insert Nil A
+
+    printfn "\nИсходное дерево:"
+    printTree tree
+
+    let newTree = mapTree duplicateString tree
+
+    printfn "\nНовое дерево:"
+    printTree newTree
 
     0
